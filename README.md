4f678afa0ae48092eaeaba315ab5db6056d174d9
l2thet <dan.w00tz@gmail.com>
2025-02-27 14:49:27 -0800

    Changing how the portnumber is defined and designing the Front End

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Create Docker image
 docker build -t executus-app .
 
 Run Docker container
-docker run -p 8081:8081 executus-app
+docker run -p 8081:8081 -e ClientPort=:8081 executus-app
--- main.go
+++ main.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-const PORTNUMBER = 8081
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
+	var PORTNUMBER = os.Getenv("ClientPort")
+	if PORTNUMBER == "" {
+		log.Fatal("ClientPort environment variable not set")
+	}
+
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// Create a handler function for the root URL
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request received: %s %s\n", r.Method, r.URL.Path)
 		http.ServeFile(w, r, "./static/index.html")
 	})
 
-	fmt.Printf("Server is running on port %d", PORTNUMBER)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", PORTNUMBER), nil); err != nil {
+	fmt.Printf("Server is running on port %s", PORTNUMBER)
+	if err := http.ListenAndServe(PORTNUMBER, nil); err != nil {
 		fmt.Println("Server is not running")
 	}
 
